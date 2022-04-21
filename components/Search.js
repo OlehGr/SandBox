dac83b5b7ad87da542657e033aea645c23a5a43b
OlehGr <93145405+OlehGr@users.noreply.github.com>
2022-04-21 13:35:21 +0300

    Delete components directory

--- components/Search.js
+++
@@ -1,9 +0,0 @@
-export default function Search(props) {
-    const change = e => props.searchInput.changeVal(e.target.value)
-
-    return (
-        <div className={'search'}>
-            <input type={'text'} value={props.searchInput.value} onChange={change} placeholder={'Поиск...'}/>
-        </div>
-    )
-}
